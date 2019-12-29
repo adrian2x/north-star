a214bfa5242c78cca0d9dc12f2d4d7065676af8c
Adrian C <adr.ca@live.com>
2019-12-28 21:39:23 -0800

    add react router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,24 @@
 import * as React from "react";
+import { Store } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { Main } from "./components/Main";
 
 import "../styles/base.scss";
 
-export class App extends React.Component {
+export interface AppProps {
+  store: Store;
+}
+
+export class App extends React.PureComponent<AppProps, {}> {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <Provider store={this.props.store}>
+        <Router>
+          <Route path="/" component={Main} />
+        </Router>
+      </Provider>
+    );
   }
 }
---
+++ src/app/components/Main.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export class Main extends React.PureComponent<{}> {
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
+}
